aa44c660336451c732bb88c490bed9f971654284
Veewong0240 <cwang18@uchicago.edu>
2023-05-09 17:07:34 -0500

    1

---
+++ java/showimg.js
@@ -0,0 +1,57 @@
+function howdy(firstName){
+    alert("Howdy " + firstName);
+}
+
+function displayMinute(){
+    var date = new Date();
+    var currentMinute = date.getMinutes();
+    
+    alert("Current minute:" + currentMinute)
+}
+
+function hideElement(){
+    var element = document.getElementById('body')
+
+    element.style.display = 'none';
+}
+
+function mapLoad(){
+    //Define the lat lon coordinate
+    var latLng = [53.4831, 2.2004];
+  
+    var mbAttr = 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    mbUrl = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
+  
+    var grayscale   = L.tileLayer(mbUrl, {id: 'mapbox/light-v9', tileSize: 512, zoomOffset: -1, attribution: mbAttr}),
+    streets  = L.tileLayer(mbUrl, {id: 'mapbox/streets-v11', tileSize: 512, zoomOffset: -1, attribution: mbAttr});
+  
+    var map = L.map('map', {
+      center: latLng,
+      zoom: 16,
+      layers: [streets]
+    });
+  
+    var baseLayers = {
+      "Grayscale": grayscale,
+      "Streets": streets
+    };
+  
+    L.control.layers(baseLayers).addTo(map);
+  
+    L.marker(latLng).addTo(map)
+    .bindPopup("<b>Manchester City<br>Etihad Stadium</b>").openPopup();
+  
+  
+  
+    //Click event
+    var popup = L.popup();
+  
+    function onMapClick(e) {
+      popup
+      .setLatLng(e.latlng)
+      .setContent("You clicked the map at " + e.latlng.toString())
+      .openOn(map);
+    }
+    map.on('click', onMapClick);
+  }
