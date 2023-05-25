505cee764173b8591ba918dadb904c22c18c1ae4
Veewong0240 <cwang18@uchicago.edu>
2023-05-25 09:44:33 -0500

    1

--- java/showimg.js
+++ java/showimg.js
@@ -22,7 +22,7 @@ function mapLoad(){
   
     var mbAttr = 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
     'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-      mbUrl = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}? access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
+    mbUrl = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}? access_token=pk.eyJ1IjoidmVld29uZyIsImEiOiJjbGkxOHNnOG0wZ2s3M2twY3FlYzYybTlzIn0.ggP6gOChOUogdbH6p36MMg';
   
     var grayscale   = L.tileLayer(mbUrl, {id: 'mapbox/light-v9', tileSize: 512, zoomOffset: -1, attribution: mbAttr}),
     streets  = L.tileLayer(mbUrl, {id: 'mapbox/streets-v11', tileSize: 512, zoomOffset: -1, attribution: mbAttr});
